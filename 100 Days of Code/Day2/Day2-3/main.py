6f5e9947bb92cc6497c7a326bf701255e137fbbc
Edgar Adrian <adick.teddy@gmail.com>
2020-12-05 23:57:42 -0600

    Day 2

---
+++ 100 Days of Code/Day2/Day2-3/main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_int = int(age)
+days = age_int * 365
+weeks = age_int * 52
+months = age_int * 12
+
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
+
+
+
+
+
+
+
+
