214d5c1ce173611e198f1a8bbb833d002f7597c8
Edgar Adrian <adick.teddy@gmail.com>
2020-12-26 18:39:13 -0600

    Day 20 Snake game part 1

---
+++ 100 Days of Code/Day20/snake_game/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.turtles = []
+
+        for position in STARTING_POSITIONS:
+            turtle_1 = Turtle("square")
+            turtle_1.color("white")
+            turtle_1.penup()
+            turtle_1.goto(position)
+            self.turtles.append(turtle_1)
+
+        self.head = self.turtles[0]
+
+    def move(self):
+        for turtle_num in range(start=len(self.turtles) - 1, stop=0, step=-1):
+            new_x = self.turtles[turtle_num - 1].xcor()
+            new_y = self.turtles[turtle_num - 1].ycor()
+            self.turtles[turtle_num].goto(new_x, new_y)
+        self.turtles[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
