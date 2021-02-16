3915b52e785f7e134fe96e4f5f0de8b43b2ba002
Edgar Adrian <adick.teddy@gmail.com>
2021-02-15 18:55:46 -0600

    Raising your own Exceptions

--- 100 Days of Code/Day30/day-30/main.py
+++ 100 Days of Code/Day30/day-30/main.py
@@ -13,4 +13,14 @@ else:
     content = file.read()
     print(content)
 finally:
-    file.close()
+    print("Complete!")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
