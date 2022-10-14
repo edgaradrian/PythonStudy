868d273a68ec0f79d84ad079d96d7ef4b48588d4
Edgar Adrián <adick.teddy@gmail.com>
2022-10-14 12:34:33 -0500

    Using Variables in Strings

--- CrashCourse/Chapter2/full_name.py
+++ CrashCourse/Chapter2/full_name.py
@@ -2,3 +2,8 @@ first_name = "edgar"
 last_name = "adrián"
 full_name = f"{first_name} {last_name}"
 print(full_name.title())
+
+print(f"Hola, {full_name.title()}")
+
+message = f"Hola, {full_name.upper()}"
+print(message)
