952bb1c28cba315fb25505d9f7a2e83808faaead
Edgar Adrián <adick.teddy@gmail.com>
2022-10-14 13:00:26 -0500

    Stripping Whitespace

--- CrashCourse/Chapter2/languages.py
+++ CrashCourse/Chapter2/languages.py
@@ -3,3 +3,13 @@ print("\tPython")
 print("Languages:\nPython\nSwift\nRust\nKotlin")
 print("Languages:\n\tPython\n\tSwift\n\tRust\n\tKotlin")
 
+favorite_language = "python "
+print(favorite_language)
+print(f"Favorite language: {favorite_language.rstrip()}")
+
+favorite_language = ' python '
+print(f"{favorite_language.rstrip()}")
+print(f"{favorite_language.lstrip()}")
+print(f"{favorite_language.strip()}")
+
+
