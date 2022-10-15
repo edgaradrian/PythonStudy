d8c0fd24a82481fb72e9db58735d6211af606c62
Edgar Adrián <adick.teddy@gmail.com>
2022-10-15 14:44:55 -0500

    Accesing Elements in a List

--- CrashCourse/Chapter3/bicycles.py
+++ CrashCourse/Chapter3/bicycles.py
@@ -1,2 +1,9 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+#To access an element in a list
+print(bicycles[0])
+
+#To look more presentable
+print(bicycles[0].title())
+
