74c69fbdf2954de2178f6f64323bdeff76eb3f6e
Edgar Adrián <adick.teddy@gmail.com>
2022-10-15 14:50:55 -0500

    Index Positions Start at 0, Not 1

--- CrashCourse/Chapter3/bicycles.py
+++ CrashCourse/Chapter3/bicycles.py
@@ -7,3 +7,9 @@ print(bicycles[0])
 #To look more presentable
 print(bicycles[0].title())
 
+#The following asks for the bicycles at index 1 and index 3:
+print(bicycles[1])
+print(bicycles[3])
+
+#the last element in a list
+print(bicycles[-1])
