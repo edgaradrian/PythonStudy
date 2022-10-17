579362ccfc211b623c26cae13e909d359e9c8f90
Edgar Adrián <adick.teddy@gmail.com>
2022-10-17 15:04:23 -0500

    Sorting a List permanently with the sort() method

---
+++ CrashCourse/Chapter3/cars.py
@@ -0,0 +1,8 @@
+#Sorting a List permanently with the sort() method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
