f134c5b2ff9b15db07f43802a19372eb03380f73
Edgar Adrián <adick.teddy@gmail.com>
2022-10-17 15:23:11 -0500

    Sorting a List temporarily with the sorted() function

--- CrashCourse/Chapter3/cars.py
+++ CrashCourse/Chapter3/cars.py
@@ -6,3 +6,16 @@ print(cars)
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort(reverse=True)
 print(cars)
+
+#Sorting a List temporarily with the sorted() function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
