6b5f1a714c7f587b348cd40dd5ca5ae405e31211
Edgar Adrián <adick.teddy@gmail.com>
2022-10-26 13:34:38 -0500

    Doing Something After a for Loop

--- CrashCourse/Chapter4/magicians.py
+++ CrashCourse/Chapter4/magicians.py
@@ -1,4 +1,5 @@
 magicians = ['abril', 'alissa', 'astrid']
 for magician in magicians:
     print(magician.title())
-    print(f"{magician.title()} ese fue un gran truco")
+    print(f"{magician.title()} ese fue un gran truco\n")
+print("Gracias a todos, fue un gran show.")
