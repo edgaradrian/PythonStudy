c9f64b2f0bcb94980f9f298d9aabbf4e1470c131
Edgar Adrián <adick.teddy@gmail.com>
2022-11-01 15:39:50 -0600

    Copying a List

---
+++ CrashCourse/Chapter4/foods.py
@@ -0,0 +1,8 @@
+my_foods = ["tacos tripa", "pizza", "birria"]
+friend_foods = my_foods[:]
+
+print("Mis comidas favoritas:")
+print(my_foods)
+
+print("Las comidas favoritas de mis amigos:")
+print(friend_foods)
