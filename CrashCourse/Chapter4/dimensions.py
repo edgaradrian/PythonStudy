b716c5d118b3c1ab8af6d1e98664f1b25727cc0d
Edgar Adrián <adick.teddy@gmail.com>
2022-11-01 16:20:16 -0600

    Writing Over a Tuple

--- CrashCourse/Chapter4/dimensions.py
+++ CrashCourse/Chapter4/dimensions.py
@@ -1,3 +1,11 @@
 dimensions = (100, 20)
 print(dimensions[0])
 print(dimensions[1])
+
+print("Original dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
