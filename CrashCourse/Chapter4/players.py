2f891a4ce6c74acc50e33de403560c20adc32c2b
Edgar Adrián <adick.teddy@gmail.com>
2022-11-01 15:31:42 -0600

    Looping Through a Slice

--- CrashCourse/Chapter4/players.py
+++ CrashCourse/Chapter4/players.py
@@ -3,3 +3,7 @@ print(players[0:3])
 print(players[1:4])
 print(players[:4])
 print(players[3:])
+
+print("Aquí están los primeros 2 jugadores del equipo")
+for player in players[:2]:
+    print(player)
