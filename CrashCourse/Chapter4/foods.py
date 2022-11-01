b00b34ff8e42c58666d9d7956d7670462abc32dc
Edgar Adrián <adick.teddy@gmail.com>
2022-11-01 15:41:06 -0600

    Adding a new food to each list

--- CrashCourse/Chapter4/foods.py
+++ CrashCourse/Chapter4/foods.py
@@ -6,3 +6,12 @@ print(my_foods)
 
 print("Las comidas favoritas de mis amigos:")
 print(friend_foods)
+
+my_foods.append('Ice cream')
+friend_foods.append('carnitas')
+
+print("Mis comidas favoritas:")
+print(my_foods)
+
+print("Las comidas favoritas de mis amigos:")
+print(friend_foods)
