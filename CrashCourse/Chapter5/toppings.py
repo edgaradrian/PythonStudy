480139e831aa63d01e999d53e03f6769495af437
Edgar Adrián <adick.teddy@gmail.com>
2022-11-06 19:12:37 -0600

    Checking for Special Items

--- CrashCourse/Chapter5/toppings.py
+++ CrashCourse/Chapter5/toppings.py
@@ -4,7 +4,7 @@ if requested_topping != 'anchovies':
     print("Hold the anchovies!")
 
 
-requested_toppings = ['mushrooms', 'extra cheese']
+requested_toppings = ['mushrooms', 'extra cheese', 'green peppers']
 
 if 'mushrooms' in requested_toppings:
     print("Adding mushrooms.")
@@ -14,3 +14,11 @@ elif 'pepperoni' in requested_toppings:
     print("Adding pepperoni")
 
 print("\nFinished making your pizza!")
+
+for topping in requested_toppings:
+    if topping == 'green peppers':
+        print("Sorry, we are out of green peppers.")
+    else:
+        print(f"Adding {topping}")
+
+print("\nFinished making your pizza!")    
