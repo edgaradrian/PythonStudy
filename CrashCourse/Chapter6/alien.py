3d35283d93086f3dc65c31a0f2586907dde66cf5
Edgar Adrián <adick.teddy@gmail.com>
2022-11-07 12:51:30 -0600

    Adding New Key-Value Pairs

--- CrashCourse/Chapter6/alien.py
+++ CrashCourse/Chapter6/alien.py
@@ -5,3 +5,9 @@ print(alien_0['points'])
 
 new_points = alien_0['points']
 print(f"I just earned {new_points} points")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 30
+alien_0['z_position'] = -2
+
+print(alien_0)
