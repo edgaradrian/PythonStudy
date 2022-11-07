c9babc7603b6479659a17dae504fa7003a945866
Edgar Adrián <adick.teddy@gmail.com>
2022-11-06 19:14:30 -0600

    Checking That a List Is Not Empty

--- CrashCourse/Chapter5/toppings.py
+++ CrashCourse/Chapter5/toppings.py
@@ -15,10 +15,15 @@ elif 'pepperoni' in requested_toppings:
 
 print("\nFinished making your pizza!")
 
-for topping in requested_toppings:
-    if topping == 'green peppers':
-        print("Sorry, we are out of green peppers.")
-    else:
-        print(f"Adding {topping}")
+requested_toppings = []
+
+if requested_toppings: 
+    for topping in requested_toppings:
+        if topping == 'green peppers':
+            print("Sorry, we are out of green peppers.")
+        else:
+            print(f"Adding {topping}")
+else:
+    print("Are you sure you want a plain pizza?")
 
 print("\nFinished making your pizza!")    
