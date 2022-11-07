0a4b6827bd2448bb124fc67250be46263c327ca5
Edgar Adrián <adick.teddy@gmail.com>
2022-11-06 18:20:55 -0600

    Ignoring Case When Checking for Equality

--- CrashCourse/Chapter5/cars.py
+++ CrashCourse/Chapter5/cars.py
@@ -5,3 +5,11 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+#Checking case for equality
+for car in cars:
+    if car.lower() == 'audi':
+        print(f"Este sí es un carro {car.title()}")
+    else:
+        print("No es un carro audi")
+
