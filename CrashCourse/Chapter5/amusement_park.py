accf5662064546ab8aa78ba9271ad25783171b29
Edgar Adrián <adick.teddy@gmail.com>
2022-11-06 19:04:42 -0600

    Omitting the else Block

--- CrashCourse/Chapter5/amusement_park.py
+++ CrashCourse/Chapter5/amusement_park.py
@@ -1,11 +1,9 @@
-age = 17
+age = 65
 if age < 4:
     price = 0
 elif age < 18:
     price = 15
-elif age < 65:
+elif age >= 65:
     price = 25
-else:
-    price = 20
 
 print(f"Your admission cost is ${price}.")
