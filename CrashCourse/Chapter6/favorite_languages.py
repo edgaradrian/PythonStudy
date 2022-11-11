cec627393ecff611bbebf8d1eaa96a01ae09eb21
Edgar Adrián <adick.teddy@gmail.com>
2022-11-10 21:41:29 -0600

    A Dictionary of Similar Objects

---
+++ CrashCourse/Chapter6/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'adrian': 'objective-c',
+    'dulce': 'c',
+    'abril': 'rust',
+    'alissa': 'kotlin'
+}
+
+language = favorite_languages['dulce'].title()
+print(f"Dulce's favorite language is {language}")
