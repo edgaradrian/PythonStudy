4b8128ddcc97309b27266b26ba3b6f1a339c8e79
Edgar Adrián <adick.teddy@gmail.com>
2022-11-10 22:01:06 -0600

    Looping Through All Key-Value Pairs

--- CrashCourse/Chapter6/favorite_languages.py
+++ CrashCourse/Chapter6/favorite_languages.py
@@ -14,3 +14,7 @@ alien_51 = {'color': 'cyan', 'speed': 'slow'}
 point_value = alien_51.get('points', 'No point value assigned.')
 print(point_value)
 
+#Looping Through All Key-Value Pairs
+for name, language_loop in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language_loop.title()}")
+
---
+++ CrashCourse/Chapter6/user.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username': 'addickto',
+    'first': 'addick',
+    'last': 'teddy'
+}
+
+for key, value in user_0.items():
+    print(f"Key: {key}")
+    print(f"Value: {value}")
